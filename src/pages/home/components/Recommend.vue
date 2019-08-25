287ade388ab29dc925d80a3c2a097757a2852fb7
unknown <2875758793@qq.com>
2019-08-26 00:07:52 +0800

    首页推荐创建

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
