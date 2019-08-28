d3b5d4c65ce6b0578d3c20e6abe940aeeb2bf57c
NCR7777 <2875758793@qq.com>
2019-08-28 21:40:28 +0800

    添加境内境外选项

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="header">
-    <router-link to="/">
-      <div class="iconfont header-back">&#xe658;</div>
-    </router-link>
+  <div>
+    <div class="header border-bottom">
+      <router-link to="/">
+        <div class="iconfont header-back">&#xe658;</div>
+      </router-link>
       城市选择
+    </div>
+    <div class="city-address">
+      <ul class="address-ul border">
+        <li class="address-item active border">境内</li>
+        <li class="address-item border">境外·港澳台</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -31,4 +39,22 @@ export default {
       font-size: .6rem
       color #fff
       padding-left:.05rem
+  .city-address
+    line-height $headerHeight
+    background $bgColor
+    padding .2rem 1rem
+    color #fff
+    .address-ul
+      overflow: hidden
+      border-radius .03rem
+      height .4rem
+      line-height .4rem
+    .address-item
+        width 50%
+        float left
+        height .4rem
+        text-align center
+    .active
+      background #fff
+      color $bgColor
 </style>
